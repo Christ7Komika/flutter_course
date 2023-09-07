07f6f9315c4b9d07fdb935d6e1098c1610a4a441
Christ7Komika <christkomika7@gmail.com>
2023-09-07 15:44:19 +0100

    Mis a jour

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-// import "package:flutter_app/screens/Dashboard.dart";
+import "package:flutter_app/screens/Dashboard.dart";
 import "package:flutter_app/screens/Home.dart";
 
 void main() {
@@ -15,6 +15,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.system,
         title: "My Flutter App",
-        home: Home());
+        home: Dashboard()
+        // home: Home());
+        );
   }
 }
